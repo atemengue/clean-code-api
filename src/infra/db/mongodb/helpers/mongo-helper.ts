57fc8d7b1644c6d2b044804ece72aeb6dbe9e180
atemengue <moafembe@gmail.com>
2023-07-10 05:23:21 +0100

    refactor: add id properties on AccountModel and correct mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,16 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, type Collection } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URI);
+    this.client = await MongoClient.connect(uri);
   },
 
   async disconnect (): Promise<void> {
     await this.client.close();
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
