c05aacfe6741e5b620982d5dc3218065d07e722e
atemengue <moafembe@gmail.com>
2023-07-11 07:34:16 +0100

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,2 @@
-import express from 'express';
-
-const app = express();
-app.listen(5050, () => { console.log('Listen on port 5050'); })
+import app from './config/app';
+app.listen(5050, () => { console.log('Listen on port 5050'); });
