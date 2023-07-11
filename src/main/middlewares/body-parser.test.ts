29e76b6876375b358a675b16ad12744b86945bde
atemengue <moafembe@gmail.com>
2023-07-11 07:44:49 +0100

    feat: ensure cors is enable

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -3,11 +3,11 @@ import app from '../config/app';
 
 describe('Body parser Middleware', () => {
   test('Should parse body as json', async () => {
-    app.post('/test-body-parser/', (req, res) => {
+    app.post('/test_body_parser/', (req, res) => {
       res.send(req.body)
     })
     await request(app)
-      .post('/test-body-parser/')
+      .post('/test_body_parser/')
       .send({ name: 'Regis' })
       .expect({ name: 'Regis' })
   })
